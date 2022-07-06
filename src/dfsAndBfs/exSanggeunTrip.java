35261b72b62d877c65f1c2fa16c85529693d13ce
yujung7768903 <yu-jung31476@naver.com>
2022-07-06 17:53:41 +0900

    Add BOJ_9372 상근이의 여행

--- src/dfsAndBfs/exSanggeunTrip.java
+++ src/dfsAndBfs/exSanggeunTrip.java
@@ -1,2 +1,24 @@
-package dfsAndBfs;public class exSanggeunTrip {
+package dfsAndBfs;
+
+import java.util.Scanner;
+
+public class exSanggeunTrip {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        int[] result = new int[t];
+        for (int i = 0; i < t; i++) {
+            int n = scanner.nextInt();
+            int m = scanner.nextInt();
+            for (int j = 0; j < m; j++) {
+                int node1 = scanner.nextInt();
+                int node2 = scanner.nextInt();
+            }
+            result[i] = n - 1;
+        }
+
+        for (int item : result) {
+            System.out.println(item);
+        }
+    }
 }
