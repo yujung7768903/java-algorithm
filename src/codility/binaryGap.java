5d5d2fa365d49e201ab1221ce044b936a170d7f6
yujung7768903 <yu-jung31476@naver.com>
2022-08-12 23:36:01 +0900

    Add codility binaryGap

--- src/codility/binaryGap.java
+++ src/codility/binaryGap.java
@@ -1,2 +1,36 @@
-package codility;public class binaryGap {
+package codility;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class binaryGap {
+    public static void main(String[] args) {
+        System.out.println(solution(32));
+    }
+
+    public static int solution(int N) {
+        // write your code in Java SE 11
+        ArrayList<Integer> gapList = new ArrayList<>();
+        int indexOfLast1 = -1;
+        String binaryN = Integer.toBinaryString(N);
+
+        System.out.println(binaryN);
+        for (int i = 0; i < binaryN.length(); i++) {
+            System.out.println("i: " + i);
+            if (binaryN.charAt(i) == '1') {
+                if (indexOfLast1 >= 0) {
+                    gapList.add(i - indexOfLast1 - 1);
+                    System.out.println("indexOfLast1: " + indexOfLast1);
+                    System.out.println(gapList.toString());
+                }
+                indexOfLast1 = i;
+            }
+        }
+
+        if (!gapList.isEmpty()) {
+            return Collections.max(gapList);
+        } else {
+            return 0;
+        }
+    }
 }
