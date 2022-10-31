81fa03fc7073541af46de6944f8b74f1b77146e3
yujung7768903 <yu-jung31476@naver.com>
2022-11-01 03:28:25 +0900

    programmers 숫자 문자열과 영단어 수정

--- src/sampleQuestions/kakao2021/numStringAndWord.java
+++ src/sampleQuestions/kakao2021/numStringAndWord.java
@@ -4,59 +4,18 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class numStringAndWord {
-    public static StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) {
-        splitString("one4seveneight");
+        System.out.println(solution("one4seveneight"));
     }
 
-    public int solution(String s) {
-        int answer = 0;
+    public static int solution(String s) {
+        String[] strNumList = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for (int i = 0; i < strNumList.length; i++) {
+            s = s.replace(strNumList[i], Integer.toString(i));
+        }
+
+        int answer = Integer.parseInt(s);
         return answer;
     }
-
-    public static void splitString(String s) {
-        Pattern alpPattern = Pattern.compile("[a-z]+|[0-9]+");
-        Matcher alpMatcher = alpPattern.matcher(s);
-//        Pattern numPattern = Pattern.compile("[0-9]+");
-//        Matcher numMatcher = numPattern.matcher(s);
-
-        while (alpMatcher.find()) {
-            String num = alpMatcher.group();
-            System.out.println(num);
-//            sb.append(num);
-//            System.out.println(sb);
-        }
-//        while (numMatcher.find()) {
-//            sb.append(numMatcher.group());
-//            System.out.println(sb);
-//        }
-    }
-
-    public static int findNum(String s) {
-        switch (s) {
-            case "zero":
-                return 0;
-            case "one":
-                return 1;
-            case "two":
-                return 2;
-            case "three":
-                return 3;
-            case "four":
-                return 4;
-            case "five":
-                return 5;
-            case "six":
-                return 6;
-            case "seven":
-                return 7;
-            case "eight":
-                return 8;
-            case "nine":
-                return 9;
-        }
-        return 9;
-    }
-
 }
