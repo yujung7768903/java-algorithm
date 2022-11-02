96d3767d2c082d1f860f6824829f4b758363e06c
yujung7768903 <yu-jung31476@naver.com>
2022-11-02 23:26:17 +0900

    programmers 숫자의 표현

---
+++ src/common/numExpression.java
@@ -0,0 +1,32 @@
+package common;
+
+public class numExpression {
+    public static int answer = 0;
+
+    public static void main(String[] args) {
+        System.out.println(solution(15));
+    }
+
+    public static int solution(int n) {
+        for (int i = 1; i <= n; i++) {
+            findComb(n, i);
+        }
+
+        return answer;
+    }
+
+    public static void findComb(int n, int start) {
+        int sum = 0;
+        while (sum <= n) {
+            if (sum == n){
+                answer++;
+                break;
+            }
+            System.out.println("i: " + start);
+            sum += start;
+            System.out.println("sum: " + sum);
+            start++;
+        }
+    }
+
+}
